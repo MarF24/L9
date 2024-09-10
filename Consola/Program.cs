7fdf8482de79126fc963ff56383cf29472dd3d4f
MarF24 <110646734+MarF24@users.noreply.github.com>
2024-09-09 23:00:12 -0300

    Add files via upload

---
+++ Consola/Program.cs
@@ -0,0 +1,22 @@
+﻿using Dominio;
+using Dominio.Models;
+using System;
+
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sistema s = Sistema.GetInstancia();
+            Socio sos = s.GetPersonas()[5] as Socio;
+            Console.WriteLine(sos.FechaUtlimoPago);
+            Console.WriteLine(sos.MesesDeuda());
+            Console.WriteLine(sos.MontoDeuda());
+            foreach (Socio soc in s.GetSociosCumplenEsteMes()) {
+                Console.WriteLine(soc.FechaNacimiento);
+            }
+            Console.ReadKey();
+        }
+    }
+}
